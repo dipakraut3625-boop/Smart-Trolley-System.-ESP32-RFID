4698fb722fdedeec3204f2c50a0a8a7579711317
DTG <dipakraut3625@gmail.com>
2026-08-10 21:40:25 +0530

    config.js change

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
-const connectDB = require("./config/db");
+const mongoose = require("mongoose");
 const path = require("path");
 const http = require("http");
 const { Server } = require("socket.io");
